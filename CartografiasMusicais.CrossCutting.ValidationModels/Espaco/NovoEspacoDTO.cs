bd2e6cadf771d96280f8bbdab6cdbfacf1ebe7d3
Marcos Ammon <marcos.ammon@gmail.com>
2020-03-21 06:15:58 -0300

    CRUD de Espacos

---
+++ CartografiasMusicais.CrossCutting.ValidationModels/Espaco/NovoEspacoDTO.cs
@@ -0,0 +1,21 @@
+﻿using CartografiasMusicais.CrossCutting.Utils;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CartografiasMusicais.CrossCutting.ValidationModels.Espaco
+{
+    public class NovoEspacoDTO
+    {
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Descrição é obrigatório")]
+        public string Descricao { get; set; }
+        public string Slug { get; set; }
+        [Display(Name = "Imagem")]
+        [ValidExtension(".jpg,.jpeg,.png,.JPG,.JPEG,.PNG")]
+        public IFormFile Imagem { get; set; }
+        public string CaminhoImagem { get; set; }
+    }
+}
---
+++ CartografiasMusicais/Areas/Admin/Controllers/EspacoController.cs
@@ -0,0 +1,109 @@
+﻿using CartografiasMusicais.Business.Context;
+using CartografiasMusicais.CrossCutting.Utils;
+using CartografiasMusicais.CrossCutting.ValidationModels.Espaco;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Slugify;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartografiasMusicais.Areas.Admin.Controllers
+{
+    [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class EspacoController : Controller
+    {
+        private CoreContext Context;
+        private readonly ISlugHelper SlugHelper;
+        private readonly IWebHostEnvironment HostingEnvironment;
+
+        public EspacoController(CoreContext context, ISlugHelper slugHelper, IWebHostEnvironment hostingEnvironment)
+        {
+            Context = context;
+            SlugHelper = slugHelper;
+            HostingEnvironment = hostingEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var model = await Context.Espacos.OrderByDescending(x => x.Id).ToListAsync();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(NovoEspacoDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                await Context.Espacos.AddAsync(new Espaco
+                {
+                    Descricao = obj.Descricao,
+                    Slug = SlugHelper.GenerateSlug(obj.Descricao).ToString(),
+                    Imagem = ((obj.Imagem != null) ? await FileService
+                                    .UploadFileAsync(obj.Imagem,
+                                                    HostingEnvironment.WebRootPath + "/imagens/",
+                                                    $"{obj.Descricao}_{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}_{Path.GetExtension(obj.Imagem.FileName)}") : null)
+                });
+                await Context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cidade = await Context.Espacos.FirstOrDefaultAsync(x => x.Id == id);
+            var model = new MudaEspacoDTO
+            {
+                Id = cidade.Id,
+                Descricao = cidade.Descricao,
+                CaminhoImagem = cidade.Imagem
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(MudaEspacoDTO obj)
+        {
+            var espaco = await Context.Espacos.FirstOrDefaultAsync(x => x.Id == obj.Id);
+            if (ModelState.IsValid && espaco != null)
+            {
+                espaco.Descricao = obj.Descricao;
+                espaco.Slug = SlugHelper.GenerateSlug(obj.Descricao).ToString();
+                if (obj.Imagem != null)
+                {
+                    espaco.Imagem = await FileService
+                                        .UploadFileAsync(obj.Imagem,
+                                                        HostingEnvironment.WebRootPath + "/imagens/",
+                                                        $"{obj.Descricao}_{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}_{Path.GetExtension(obj.Imagem.FileName)}");
+                }
+                await Context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var espaco = await Context.Espacos.FirstOrDefaultAsync(x => x.Id == id);
+
+            Context.Espacos.Remove(espaco);
+            await Context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
